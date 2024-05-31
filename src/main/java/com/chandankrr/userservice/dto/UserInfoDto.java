affc1e5e02c4753974beaccea37a3d6d3a455805
Chandan Kumar <chandankrr.91@gmail.com>
2024-05-31 11:27:14 +0530

    Create entity and dto

---
+++ src/main/java/com/chandankrr/userservice/dto/UserInfoDto.java
@@ -0,0 +1,41 @@
+package com.chandankrr.userservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserInfoDto {
+
+    @NonNull
+    @JsonProperty("user_id")
+    private String userId;
+
+    @NonNull
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @NonNull
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @NonNull
+    @JsonProperty("email")
+    private String email;
+
+    @NonNull
+    @JsonProperty("phone_number")
+    private Long phoneNumber;
+
+    @JsonProperty("profile_pic")
+    private String profilePic;
+
+}
---
+++ src/main/java/com/chandankrr/userservice/entity/UserInfo.java
@@ -0,0 +1,42 @@
+package com.chandankrr.userservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String userId;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private Long phoneNumber;
+
+    private String profilePic;
+}
